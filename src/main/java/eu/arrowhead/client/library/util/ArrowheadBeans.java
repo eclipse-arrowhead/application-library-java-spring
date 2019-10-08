bc69b505b4d609a95270617e8fc13708fef66bfb
borditamas <borditamas@gmail.com>
2019-10-08 12:33:01 +0200

    deploy client library 4.1.3.2

--- src/main/java/eu/arrowhead/client/library/util/ArrowheadBeans.java
+++ src/main/java/eu/arrowhead/client/library/util/ArrowheadBeans.java
@@ -1,14 +1,9 @@
 package eu.arrowhead.client.library.util;
 
-import java.util.Map;
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import eu.arrowhead.client.library.ArrowheadService;
-import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.SSLProperties;
 import eu.arrowhead.common.http.HttpService;
 
@@ -20,9 +15,6 @@ public class ArrowheadBeans {
 	
 	@Autowired
 	private static ArrowheadService arrowheadService;
-	
-	@Resource(name = CommonConstants.ARROWHEAD_CONTEXT)
-	private static Map<String,Object> arrowheadContext;
 	
 	@Autowired
 	private static SSLProperties sslProperties;
@@ -39,14 +31,6 @@ public class ArrowheadBeans {
 	 */
 	public static ArrowheadService getArrowheadService() {
 		return arrowheadService;
-	}
-	
-	//-------------------------------------------------------------------------------------------------
-	/** 
-	 * @return the ArrowheadContext spring managed bean
-	 */
-	public static Map<String,Object> getArrowheadContext() {
-		return arrowheadContext;
 	}
 	
 	//-------------------------------------------------------------------------------------------------
